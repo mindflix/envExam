a2ad3bae277e179d47ce69a51cd9b331de997da8
mindflix <nyco.demol@gmail.com>
2023-01-31 17:58:01 +0100

    new

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let answer = document.querySelectorAll(".answer");
 let qtext = document.querySelectorAll("div.qtext");
+let answer = document.querySelectorAll(".answer");
 q = [];
 r = [];
 qtext.forEach(function (e) {
   q.push(e.innerText);
 });
 answer.forEach(function (e) {
-  r.push(e.innerText);
+  r.push(e.innerText.split('\n\n'));
 });
 
 console.log(q, r);
